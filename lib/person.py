8314ff486053e48baf80cb3780756a313bb39873
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-11-28 17:31:32 +0300

    learned how to add in instance using class by creating a function with self

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
     pass
+talker = Person()
+talker.talk()
+walker = Person()
+walker.walk()
